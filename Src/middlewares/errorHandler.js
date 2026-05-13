ff6937e1619f7f391bb47ca317c6727e18afd6d0
Nuella <ellaedu04@gmail.com>
2026-05-13 12:34:14 +0100

    final upload with storage

--- Src/middlewares/errorHandler.js
+++ Src/middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
   const status = err.status || 500;
+
+  if (err instanceof multer.MulterError); {
+    res.status(400).json('Invalid file type or File too large');
+  }
   res.status(status).json({ error: err.message });
 };
 
--- Src/routes/user.route.js
+++ Src/routes/user.route.js
@@ -8,9 +8,9 @@ router.post('/sign-up', registerUser);
 
 router.post('/login', loginUser);
 
-const upload = multer({dest: 'uploads/' }); //Auto creates folder
+const upload = multer({dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 * 1024 }}); //Auto creates folder
 
-router.post('/picture', upload.single("picture"), (req, res) => {
+router.post('/picture', upload.array('picture', 5), (req, res) => {
   console.log('file', req.file);
   console.log('body', req.body);
   res.send('Upload successful');
